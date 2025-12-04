94b3bd0e35fa856b9a33d5aade015fd676db0ac2
poojapanchal300798 <poojapanchal3007@gmail.com>
2025-12-05 00:58:38 +0200

    server change

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,7 @@ const path = require("path");
 require("dotenv").config();
 
 const authRoutes = require("./routes/auth");
-const movieRoutes = require('./routes/movies');
-const showRoutes = require('./routes/shows');
-const bookingRoutes = require('./routes/bookings');
-const errorMiddleware = require('./middlewares/errorMiddleware');
-
+const showRoutes = require("./routes/showRoutes");
 
 const app = express();
 
@@ -26,18 +22,14 @@ app.use(express.json());
 // API ROUTES
 app.use("/auth", authRoutes);
 app.use("/api/shows", showRoutes);
-app.use('/api/movies', movieRoutes);
-app.use('/api/shows', showRoutes);
-app.use('/api/bookings', bookingRoutes);
 
-// Error handling middleware
-app.use(errorMiddleware);
+// ---------------- SERVE FRONTEND FOR AZURE ----------------
+const frontendPath = path.join(__dirname, "frontend", "build");
 
-// ---------------- SERVE FRONTEND (CORRECT PATH) ----------------
-app.use(express.static(path.join(__dirname, "../frontend/build")));
+app.use(express.static(frontendPath));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../frontend/build/index.html"));
+  res.sendFile(path.join(frontendPath, "index.html"));
 });
 
 // ---------------- START SERVER ----------------
